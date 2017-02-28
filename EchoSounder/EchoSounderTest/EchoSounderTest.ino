699793920ccb5ec20ae1e4185c4ab2fc3e91bdd8
enordstrand <eiriknordstrand@gmail.com>
2017-02-28 18:36:17 +0100

    Delayed the time

--- EchoSounder/EchoSounderTest/EchoSounderTest.ino
+++ EchoSounder/EchoSounderTest/EchoSounderTest.ino
@@ -25,5 +25,5 @@ void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(101);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(101);              // wait for a second
+  delay(1011);              // wait for a second
 }
