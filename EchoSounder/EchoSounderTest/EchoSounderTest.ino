bed310bca8b5941610517c3bf3ebefc6973d202e
enordstrand <eiriknordstrand@gmail.com>
2017-02-28 18:53:36 +0100

    TG-3 Changed to an example code

--- EchoSounder/EchoSounderTest/EchoSounderTest.ino
+++ EchoSounder/EchoSounderTest/EchoSounderTest.ino
@@ -1,29 +1,66 @@
 /*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
-
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ And modified further by ScottC here: http://arduinobasics.blogspot.com.au/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html
+ on 10 Nov 2012.
  */
 
 
-// the setup function runs once when you press reset or power the board
+#define echoPin 3 // Echo Pin
+#define trigPin 4 // Trigger Pin
+#define voltagePin 5 // Echo Pin
+#define gndPin 2 // Trigger Pin
+#define LEDPin 12 // Onboard LED
+
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(12, OUTPUT);
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(voltagePin, OUTPUT);
+ digitalWrite(voltagePin, HIGH);
+ pinMode(gndPin, OUTPUT);
+ digitalWrite(gndPin, LOW);
+ pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(101);              // wait for a second
-  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
-  delay(101);              // wait for a second
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+ 
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+ 
+ if (distance >= maximumRange || distance <= minimumRange){
+ /* Send a negative number to computer and Turn LED ON 
+ to indicate "out of range" */
+ Serial.println("-1");
+ digitalWrite(LEDPin, HIGH); 
+ }
+ else {
+ /* Send the distance to the computer using Serial protocol, and
+ turn LED OFF to indicate successful reading. */
+ Serial.println(distance);
+ digitalWrite(LEDPin, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
 }
